75a6b4f8a6b12d580acabfc1ced1ad0e64d34be0
ricsimarta <ricsimarta@gmail.com>
2022-05-23 23:23:28 +0200

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,49 @@
 import React from 'react'
+import ReCAPTCHA from 'react-google-recaptcha'
 
 function Contact() {
+  function captcha(value) {
+    const isHuman = fetch('https://www.google.com/recaptcha/api/siteverify', {
+      method: 'POST',
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/x-www-form-urlencoded; charset=utf-8"
+      },
+      body: `secret=6Lf8shIgAAAAADX-EzLHxxefk9kTpOHtoq8ozAbf&response=${value}`
+    })
+      .then(res => res.json())
+      .then(json => json.success)
+      .catch(err => {
+        throw new Error(`Error in Google Siteverify API. ${err.message}`)
+      })
+    
+    if (value === null || !isHuman) {
+      throw new Error('You are a robot')
+    }
+
+    console.log('success');
+  }
+
   return (
-    <section className='Contact'>
+    <section id='Contact'>
       <form>
         <p className='contact'>Contact me!</p>
 
-        <input type='text' placeholder='Your name' />
-        <input type='text' placeholder='Title of the message' />
-        <input className='message' type='text' placeholder='Your message' />
-        <button onClick={_ => { }}>Send</button>
+        <input name='name' type='text' placeholder='Your name' />
+        <input name='title' type='text' placeholder='Title of the message' />
+        <textarea className='message' name='message' type='text' placeholder='Your message' />
+        <button type='button' onClick={e => {
+          e.preventDefault();
+        }}>Send</button>
       </form>
+
+      <div className='captcha'>
+        <ReCAPTCHA
+          sitekey='6Lf8shIgAAAAADX-EzLHxxefk9kTpOHtoq8ozAbf'
+          onChange={captcha}
+          theme='dark'
+        />
+      </div>
     </section>
   )
 }
