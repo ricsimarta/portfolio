d3511e1cbc269802e54c80fcdfee1b67fe91e87a
ricsimarta <ricsimarta@gmail.com>
2022-05-24 12:58:28 +0200

    grammar again

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ function About() {
           <span>Hi! I am Richard Marta, a frontend developer born in Serbia, but living in Hungary.</span>
           <span>I fell in love with coding in high school, where I was learning C# and old school HTML and CSS. After finishing high school in Serbia, I moved to Hungary at the age of 19. That's when I drifted away from IT, and started working in other fields, but at the age of 26, I decided to get back on the road and started a bootcamp at Codecool.</span>
           <span>After 4 months of tears and sweat, I became a full fledged frontend developer, with the knowledge of modern frameworks and practices, like Javascript, NodeJS, ExpressJS and React. Now I'm looking forward to start my journey and build my career in the maze of frontend developing.</span>
-          <span>Outside of coding, I am deeply into hardware and software. I like to build PC's, install and maintain them, and even do smaller repairs as well. I enjoy playing games, watch movies and series, and learning to play guitar. I think of myself as a cheerful and motivated guy, and I think with a little humour, everything can be better,</span>
+          <span>Outside of coding, I am deeply into hardware and software. I like to build PC's, install and maintain them, and even do smaller repairs as well. I enjoy playing games, watching movies and series, and learning to play guitar. I think of myself as a cheerful and motivated guy, and I think with a little humour, everything can be better,</span>
         </p>
       </div>
       <button className='cv' type='button' onClick={e => {
